f05c7927e53b96f36686e959c9e0206ffb41b0a8
Jose-Lucena-Rivera <jose.lucena2@upr.edu>
2024-04-14 15:36:12 -0400

    made changes again to app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders login text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Login/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginElement = screen.getByText('Login', { selector: 'h2' });
+  expect(loginElement).toBeInTheDocument();
 });
