b5dabaad3647c72ec8821d7d08fe90f0e89742e1
Jose-Lucena-Rivera <jose.lucena2@upr.edu>
2024-05-02 11:51:41 -0400

    test 1 route login

--- src/Login.js
+++ src/Login.js
@@ -51,7 +51,7 @@ const Login = () => {
     }
 
     try {
-        const response = await fetch('http://localhost:5000/login', {
+        const response = await fetch('https://boyaslacatalana-api.azurewebsites.net/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
