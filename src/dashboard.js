393b372102d273ad0139f4018c6ec4226b08af1d
Jose-Lucena-Rivera <jose.lucena2@upr.edu>
2024-05-08 11:40:32 -0400

    auto refresh when sign out

--- src/dashboard.js
+++ src/dashboard.js
@@ -27,9 +27,11 @@ const Dashboard = () => {
   
   useEffect(() => {
     // Check if the user is logged in (i.e., if there's a token in local storage)
+    
     const token = localStorage.getItem('token');
     if (!token) {
       // If no token found, redirect the user to the login page
+      window.location.reload();
       window.location.href = '/';
     }
   }, []);
