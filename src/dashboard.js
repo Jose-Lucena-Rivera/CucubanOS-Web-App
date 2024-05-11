8afd35e4d6ec5fd22a7bae4b2c027a7f45a32d3d
Jose-Lucena-Rivera <jose.lucena2@upr.edu>
2024-05-11 18:16:06 -0400

    fixed deploy route

--- src/dashboard.js
+++ src/dashboard.js
@@ -815,7 +815,7 @@ useEffect(() => {
     };
   
     try {
-      const response = await fetch('http://127.0.0.1:5000/deploy', {
+      const response = await fetch('https://boyaslacatalana-api.azurewebsites.net/deploy', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
